5ad9e4949d6384ec4bb85f67b849388c05599f40
Diko Parvanov <diko.parvanov@canonical.com>
2026-02-05 14:40:59 +0200

    Added error handling and libraries

--- src/charm.py
+++ src/charm.py
@@ -21,6 +21,7 @@ class ForgejoRunnerCharmCharm(ops.CharmBase):
         super().__init__(framework)
         framework.observe(self.on.install, self._on_install)
         framework.observe(self.on.start, self._on_start)
+        framework.observe(self.on.update_status, self._on_update_status)
         framework.observe(self.on['register-runner'].action, self._on_register_runner)
 
     def _on_install(self, event: ops.InstallEvent):
@@ -31,22 +32,38 @@ class ForgejoRunnerCharmCharm(ops.CharmBase):
 
     def _on_start(self, event: ops.StartEvent):
         """Handle start event."""
-        self.unit.status = ops.MaintenanceStatus("Starting workload")
-        forgejo_runner.start()
+        self.unit.status = ops.ActiveStatus("Please run 'register-runner' action to register the runner")
         version = forgejo_runner.get_version()
         if version is not None:
             self.unit.set_workload_version(version)
-        self.unit.status = ops.ActiveStatus()
 
     def _on_register_runner(self, event: ops.ActionEvent):
         """Handle register-runner event."""
         self.unit.status = ops.MaintenanceStatus("Registering runner")
         try:
-            forgejo_runner.register_runner(event.params)
-            self.unit.status = ops.ActiveStatus("runner registered")
+            host = event.params["host"]
+            result = forgejo_runner.register_runner(event.params)
+            if result:
+                event.set_results({"result": f"Runner registered successfully at {host}"})
+                self.unit.status = ops.ActiveStatus(f"runner registered at {host}")
+            else:
+                err_msg = f"Failed to register runner. Check host or credentials"
+                logger.error(err_msg)
+                event.fail(err_msg)
+                self.unit.status = ops.BlockedStatus(f"failed to register runner at {host}")
         except Exception as e:
-            logger.error(f"Failed to register runner: {e}")
+            exp_msg = f"Failed to register runner: {e}"
+            logger.error(exp_msg)
+            event.fail(exp_msg)
             self.unit.status = ops.BlockedStatus("failed to register runner")
+
+    def _on_update_status(self, event: ops.UpdateStatusEvent):
+        """Handle update-status event."""
+        if not forgejo_runner.is_service_running():
+            self.unit.status = ops.BlockedStatus("forgejo-runner service not running")
+        else:
+            host = forgejo_runner.get_host()
+            self.unit.status = ops.ActiveStatus(f"runner registered at {host}")
 
 
 if __name__ == "__main__":  # pragma: nocover
--- src/forgejo_runner.py
+++ src/forgejo_runner.py
@@ -7,30 +7,58 @@ The intention is that this module could be used outside the context of a charm.
 """
 
 import logging
+import subprocess
 import os
+from charms.operator_libs_linux.v1.systemd import service_running
+
 
 logger = logging.getLogger(__name__)
 
+SYSTEMD_SERVICE = "snap.forgejo-runner.forgejo-runner.service"
 
 def install() -> None:
     """Install the forgejo-runner snap and connect to docker intrefaces."""
-    os.system("snap install forgejo-runner --channel edge")
-    os.system("snap connect forgejo-runner:docker docker:docker-daemon")
-    os.system("snap connect forgejo-runner:docker-executables docker:docker-executables")
-
-
-def start() -> None:
-    """Start the workload (by running a commamd, for example)."""
-    pass
+    # Install the forgejo-runner snap from the edge channel
+    subprocess.run(["snap", "install", "forgejo-runner", "--channel", "edge"], check=True)
+    # Connect the required interfaces
+    subprocess.run(["snap", "connect", "forgejo-runner:docker", "docker:docker-daemon"], check=True)
+    # Connect the docker-executables interface
+    subprocess.run(["snap", "connect", "forgejo-runner:docker-executables", "docker:docker-executables"], check=True)
 
 def get_version() -> str | None:
     """Get the running version of the workload."""
-    # You'll need to implement this function (or remove it if not needed).
+    try:
+        result = subprocess.run(
+            ["/snap/forgejo-runner/current/bin/forgejo-runner", "--version"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        version = result.stdout.strip()
+        version = version.replace("forgejo-runner version v", "")
+        return version
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to get forgejo-runner version: {e}")
     return None
 
-def register_runner(params):
+def register_runner(params) -> bool:
     """Register the runner to a forgejo instance."""
     host = params["host"]
     secret = params["secret"]
     cmd = f"snap set forgejo-runner host=\"{host}\" secret=\"{secret}\""
-    os.system(cmd)
+    subprocess.run(cmd, shell=True, check=True)
+    return is_service_running()
+
+def is_service_running() -> bool:
+    """Check if the forgejo-runner service has failed."""
+    return service_running(SYSTEMD_SERVICE)
+
+def get_host() -> str:
+    """Get the configured host for the runner."""
+    result = subprocess.run(
+        ["snap", "get", "forgejo-runner", "host"],
+        check=True,
+        capture_output=True,
+        text=True
+    )
+    return result.stdout.strip()
